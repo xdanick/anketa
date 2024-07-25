2bfa37d6c815623c445276fa0586099bf63a3608
xdanick <153621567+xdanick@users.noreply.github.com>
2024-07-25 21:27:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-document.getElementById('surveyForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('surveyForm').addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-    const form = event.target;
-    const formData = new FormData(form);
+        const form = event.target;
+        const formData = new FormData(form);
 
-    try {
-        const response = await fetch('https://4395-37-151-129-32.ngrok-free.app/upload', { // Убедитесь, что вы используете правильный URL ngrok
-            method: 'POST',
-            body: formData
-        });
+        try {
+            const response = await fetch('http://localhost:3000/upload', {
+                method: 'POST',
+                body: formData
+            });
 
-        if (!response.ok) {
-            throw new Error('Ошибка при отправке формы');
+            if (!response.ok) {
+                throw new Error('Ошибка при отправке формы');
+            }
+
+            const result = await response.json();
+            alert(result.message);
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Ошибка при отправке формы');
         }
-
-        const result = await response.json();
-        alert(result.message);
-    } catch (error) {
-        console.error('Error:', error);
-        alert('Ошибка при отправке формы');
-    }
+    });
 });
