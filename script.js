f23b45f9d9fccd7f526c37e5db37abf2e519151a
xdanick <153621567+xdanick@users.noreply.github.com>
2024-07-25 22:09:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('surveyForm').addEventListener('submit', async (event) => {
-        event.preventDefault();
+document.getElementById('surveyForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-        const form = event.target;
-        const formData = new FormData(form);
+    const form = event.target;
+    const formData = new FormData(form);
 
-        try {
-            const response = await fetch('http://localhost:3000/upload', {
-                method: 'POST',
-                body: formData
-            });
+    try {
+        const response = await fetch('/upload', { // Путь к вашему серверу
+            method: 'POST',
+            body: formData
+        });
 
-            if (!response.ok) {
-                throw new Error('Ошибка при отправке формы');
-            }
-
-            const result = await response.json();
-            alert(result.message);
-        } catch (error) {
-            console.error('Error:', error);
-            alert('Ошибка при отправке формы');
+        if (!response.ok) {
+            throw new Error('Ошибка при отправке формы');
         }
-    });
+
+        const result = await response.json();
+        alert(result.message);
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Ошибка при отправке формы');
+    }
 });
