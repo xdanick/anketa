a4c8ba0e0ba890a75a87b34514b4b1d5d81fd8fc
xdanick <153621567+xdanick@users.noreply.github.com>
2024-07-25 16:07:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('surveyForm').addEventListener('submit', async (event) =
     const formData = new FormData(form);
 
     try {
-        const response = await fetch('https://abcd1234.ngrok.io/submit', {
+        const response = await fetch('https://340e-37-151-129-32.ngrok-free.app', {
             method: 'POST',
             body: formData
         });
